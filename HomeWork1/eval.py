6895be69403d4a1423a2bbd3390a497f72df8d58
Stylechick <92633521+Stylechick@users.noreply.github.com>
2022-12-27 15:16:29 +0800

    Update eval.py

--- HomeWork1/eval.py
+++ HomeWork1/eval.py
@@ -15,13 +15,19 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 from PIL import Image
 from torch import nn
-
 from model import classifer
 from model import Mynn
+import win32api, win32con
+import random
 
-mymodel = torch.load('reg_49.pth')
+mymodel = torch.load('resnet_49.pth')
 print(mymodel)
-img_path = r'E:\face_classfication\BitmojiDataset_Sample\trainimages\0006.jpg'
+rannum = random.randint(0, 2999)
+strnum = str(rannum)
+while len(strnum) < 4:
+    strnum = '0' + strnum
+img_path = r'E:\face_classfication\BitmojiDataset_Sample\trainimages'
+img_path = img_path + '/' + strnum + '.jpg'
 img_pil = Image.open(img_path)
 img_pil = img_pil.resize((224, 224))
 tensor_trans = transforms.ToTensor()
@@ -32,3 +38,7 @@ img_tensor = img_tensor.cuda()
 mymodel.eval()
 output = mymodel(img_tensor)
 print(output)
+evl = 'female' if output[0][0] > output[0][1] else 'male'
+print(evl)
+win32api.MessageBox(0, evl, "结果",win32con.MB_OK)
+img_pil.show()
